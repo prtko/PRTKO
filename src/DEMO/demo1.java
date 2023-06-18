b48c42656f76afb41ffc6b1c4e873098172a915f
BLOOD EAGLE <prateek30041999bajpai@gmail.com>
2023-06-18 14:03:59 +0530

    this is first commit

---
+++ src/DEMO/demo1.java
@@ -0,0 +1,12 @@
+package DEMO;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+		System.out.printf("the sum of numbers is ");
+		int i= 15+15;
+		System.out.print(i);
+
+	}
+
+}
